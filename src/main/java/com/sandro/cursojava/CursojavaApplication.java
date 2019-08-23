f68d84de766ad9191843e279d760a2652b2f5ec5
Sandro Alliz <sandroalliz@gmail.com>
2019-08-23 19:02:54 -0300

    Projeto criado

---
+++ src/main/java/com/sandro/cursojava/CursojavaApplication.java
@@ -0,0 +1,13 @@
+package com.sandro.cursojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursojavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandro/cursojava/CursojavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sandro.cursojava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursojavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
